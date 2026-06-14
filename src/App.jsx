97e35ac3bb4bb1b2dd4233029cdb3c12c6ac813f
harshiththummala08-sys <harshiththummala08@gmail.com>
2026-06-14 09:49:06 +0530

    Set router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
  import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Workouts from "./pages/Workouts";
@@ -7,17 +8,21 @@ import WorkoutDetails from "./pages/WorkoutDetails";
 import Schedule from "./pages/Schedule";
 import Profile from "./pages/Profile";
 
+
 import ProtectedRoute from "./routes/ProtectedRoute";
+
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
+
 
         <Route
           path="/"
           element={<Login />}
         />
+
 
         <Route
           path="/dashboard"
@@ -28,45 +33,10 @@ function App() {
           }
         />
 
+
         <Route
           path="/workouts"
           element={
             <ProtectedRoute>
               <Workouts />
             </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/workouts/:id"
-          element={
-            <ProtectedRoute>
-              <WorkoutDetails />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/schedule"
-          element={
-            <ProtectedRoute>
-              <Schedule />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
