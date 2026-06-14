156fac95b445eb9b3a2273ec20e2c0c9ec4964b5
harshiththummala08-sys <harshiththummala08@gmail.com>
2026-06-14 09:51:00 +0530

    Fix App routes for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
- import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
@@ -8,21 +7,13 @@ import WorkoutDetails from "./pages/WorkoutDetails";
 import Schedule from "./pages/Schedule";
 import Profile from "./pages/Profile";
 
-
 import ProtectedRoute from "./routes/ProtectedRoute";
-
 
 function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
-
-
-        <Route
-          path="/"
-          element={<Login />}
-        />
-
+        <Route path="/" element={<Login />} />
 
         <Route
           path="/dashboard"
@@ -33,10 +24,44 @@ function App() {
           }
         />
 
-
         <Route
           path="/workouts"
           element={
             <ProtectedRoute>
               <Workouts />
             </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/workouts/:id"
+          element={
+            <ProtectedRoute>
+              <WorkoutDetails />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/schedule"
+          element={
+            <ProtectedRoute>
+              <Schedule />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
