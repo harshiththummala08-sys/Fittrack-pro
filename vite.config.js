f0d55f27eabf5369bec707e08c50df81e68b8ae8
harshiththummala08-sys <harshiththummala08@gmail.com>
2026-06-16 11:33:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Fittrack-pro/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: '/',
 })
